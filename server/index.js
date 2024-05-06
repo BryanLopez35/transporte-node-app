0127053519160b09b199735568d72510f495a7f5
Bryan <bryanantoniolopez@udetijuana.edu.mx>
2024-05-06 13:03:49 -0700

    FIX: SERVER SETTINGS

--- server/index.js
+++ server/index.js
@@ -1,22 +1,13 @@
 // server/index.js
-const path = require("path");
+
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Hacer que node sirva los archivos de nuestro app React
-app.use(express.static(path.resolve(__dirname, "../pwa-transporte/build")));
-
-// Manejar las peticiones GET en la ruta /api
 app.get("/api", (req, res) => {
-  res.json({ message: "Hola Mundo desde el Servidor!" });
-});
-
-// Todas las peticiones GET que no hayamos manejado en las líneas anteriores retornaran nuestro app React
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../pwa-transporte/build", "index.html"));
+  res.json({ message: "Hola desde el servidor!" });
 });
 
 app.listen(PORT, () => {

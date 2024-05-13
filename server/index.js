0b2c02beb0dca936b62798eec4e46cb63f39498c
Bryan <bryanantoniolopez@udetijuana.edu.mx>
2024-05-13 10:08:02 -0700

    Update Server Data

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 // server/index.js
 
+const path = require("path");
 const express = require("express");
+const app = express();
+
+// Configurar Express para servir archivos estáticos desde la carpeta 'client/build'
+app.use(express.static(path.join(__dirname, "../pwa-transporte/build")));
+
+const transportRoutes = require("./data/TransportRoutes.jsx");
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
-
 app.get("/api", (req, res) => {
   res.json({ message: "Hola desde el servidor!" });
+});
+
+// Se obtiene la lista de rutas
+app.get("/api/routes", (req, res) => {
+  res.json(transportRoutes);
 });
 
 app.listen(PORT, () => {
